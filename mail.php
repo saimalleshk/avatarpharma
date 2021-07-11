3afa582b746294494a0ae58ea8fb7cb456281011
SaiMallesh <45413132+saimalleshk@users.noreply.github.com>
2021-07-11 17:59:21 +0530

    Update and rename main.php to mail.php

--- mail.php
+++ mail.php
@@ -5,11 +5,11 @@ $phonenumber= $_POST['phonenumber'];
 $requirement= $_POST['requirement'];
 $requirement= $_POST['requirement'];
 $comment= $_POST['comment'];
-$to = "knvrajesh@gmail.com";
+$to = "avatarbiopharma@gmail.com";
 $subject = "Mail From website";
 $txt ="Name = ". $name . "\r\n  Mobile = " . $phonenumber . "\r\n Requirement =" . $requirement . "\r\n Comment= " . $comment;
 $headers = "From: noreply@avatarbiopharma.com" . "\r\n" .
-"CC: mallesh2008k@gmail.com";
+"CC: knvrajesh@gmail.com";
 if($email!=NULL){
     mail($to,$subject,$txt,$headers);
 }
